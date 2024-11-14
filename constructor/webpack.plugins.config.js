49bfbd3eb582cea2f7c1805d7abe263130623fd9
Studio Champ Gauche <comptes@champgauche.studio>
2024-11-13 19:00:40 -0500

    Webpack for wordpress theme and plugins

---
+++ constructor/webpack.plugins.config.js
@@ -0,0 +1,174 @@
+import path from 'path';
+import { fileURLToPath } from 'url';
+import CopyPlugin from 'copy-webpack-plugin';
+import TerserPlugin from 'terser-webpack-plugin';
+import ImageMinimizerPlugin from 'image-minimizer-webpack-plugin';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+const plugins = [
+	'scg-admin-style-script',
+	'scg-core'
+];
+
+
+const main = {
+	cache: false,
+	entry: plugins.reduce((entries, pluginName) => {
+
+		entries[pluginName] = [
+			`../src/back/plugins/${pluginName}/js/App.jsx`,
+			`../src/back/plugins/${pluginName}/scss/App.scss`,
+			`../src/back/plugins/${pluginName}/medias/audios/audios.js`,
+			`../src/back/plugins/${pluginName}/medias/fonts/fonts.js`,
+			`../src/back/plugins/${pluginName}/medias/images/images.js`,
+			`../src/back/plugins/${pluginName}/medias/videos/videos.js`,
+		];
+
+		return entries;
+
+	}, {}),
+	output: {
+		filename: '[name]/assets/js/main.min.js',
+		path: path.resolve(__dirname, '../dist/admin/wp-content/plugins/')
+	},
+	module: {
+		rules: [
+			{
+				test: /\.jsx?$/,
+				exclude: /node_modules/,
+				use: {
+					loader: 'babel-loader',
+				},
+			},
+			{
+				test: /\.s[ac]ss$/i,
+				use: [
+					{
+						loader: 'file-loader',
+						options: {
+							name: (file) => {
+
+
+								const index = plugins.findIndex(plugin => file.includes(plugin));
+								const extensionName = plugins[index];
+
+								return `${extensionName}/assets/css/main.min.css`;
+							},
+						}
+					},
+					'sass-loader'
+				],
+			},
+			{
+				test: /\.(png|jpg|jpeg|gif|svg|webp)$/i,
+				use: [
+					{
+						loader: 'file-loader',
+						options: {
+							name: (file) => {
+
+								const index = plugins.findIndex(plugin => file.includes(plugin));
+								const extensionName = plugins[index];
+
+								return `${extensionName}/assets/images/[name].[ext]`;
+							}
+						}
+					}
+				]
+			},
+			{
+				test: /\.(mp4)$/i,
+				use: [
+					{
+						loader: 'file-loader',
+						options: {
+							name: (file) => {
+
+								const index = plugins.findIndex(plugin => file.includes(plugin));
+								const extensionName = plugins[index];
+
+								return `${extensionName}/assets/videos/[name].[ext]`;
+							}
+						}
+					}
+				]
+			},
+			{
+				test: /\.(mp3)$/i,
+				use: [
+					{
+						loader: 'file-loader',
+						options: {
+							name: (file) => {
+
+								const index = plugins.findIndex(plugin => file.includes(plugin));
+								const extensionName = plugins[index];
+
+								return `${extensionName}/assets/audios/[name].[ext]`;
+							}
+						}
+					}
+				]
+			},
+			{
+				test: /\.(woff|woff2|eot|ttf|otf)$/i,
+				use: [
+					{
+						loader: 'file-loader',
+						options: {
+							name: (file) => {
+
+								const index = plugins.findIndex(plugin => file.includes(plugin));
+								const extensionName = plugins[index];
+
+								return `${extensionName}/assets/fonts/[name].[ext]`;
+							}
+						}
+					}
+				]
+			},
+		],
+	},
+	plugins: [
+		new CopyPlugin({
+			patterns: [
+				...plugins.map(pluginName => ({
+			        from: path.resolve(__dirname, `../src/back/plugins/${pluginName}/template`),
+			        to: path.resolve(__dirname, `../dist/admin/wp-content/plugins/${pluginName}`),
+			        noErrorOnMissing: true,
+			    }))
+			]
+		}),
+	],
+	optimization: {
+		minimizer: [
+			new ImageMinimizerPlugin({
+				minimizer: {
+					implementation: ImageMinimizerPlugin.imageminMinify,
+					options: {
+						plugins: [
+							['gifsicle', { interlaced: true }],
+							['jpegtran', { progressive: true }],
+							['mozjpeg', { quality: 75 }],
+							['optipng', { optimizationLevel: 5 }],
+							['pngquant', { quality: [0.65, 0.90], speed: 4, }],
+							['svgo', { plugins: [{ name: 'preset-default', params: { overrides: { removeViewBox: false }}}]}]
+						],
+					},
+				},
+			}),
+			new TerserPlugin()
+		],
+	},
+	resolve: {
+		modules: [
+			path.resolve(__dirname, 'node_modules')
+		],
+		extensions: ['.js', '.jsx']
+	}
+};
+
+
+export default main;
---
+++ src/back/plugins/scg-admin-style-script/medias/images/images.js
@@ -0,0 +1,2 @@
+'use strict'
+import image1 from './depth1/depth2/depth3/rect.jpg'
