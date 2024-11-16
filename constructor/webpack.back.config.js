cd219050252818bf6bdc9566e31e5b16362b7f03
Studio Champ Gauche <comptes@champgauche.studio>
2024-11-16 05:30:05 -0500

    Build, watch or prod what you want

---
+++ constructor/webpack.back.config.js
@@ -0,0 +1,9 @@
+import theme from './webpack.theme.config.js';
+import plugins from './webpack.plugins.config.js';
+import muPlugins from './webpack.mu-plugins.config.js';
+
+export default [
+	theme,
+	plugins,
+	muPlugins
+];
