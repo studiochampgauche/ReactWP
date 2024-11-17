4f95696c09f06558e3486ff886559d9c8335bf70
Studio Champ Gauche <comptes@champgauche.studio>
2024-11-17 02:13:50 -0500

    Create multiple theme

--- constructor/webpack.back.config.js
+++ constructor/webpack.back.config.js
@@ -1,9 +1,9 @@
-import theme from './webpack.theme.config.js';
+import themes from './webpack.themes.config.js';
 import plugins from './webpack.plugins.config.js';
 import muPlugins from './webpack.mu-plugins.config.js';
 
 export default [
-	theme,
+	themes,
 	plugins,
 	muPlugins
 ];
