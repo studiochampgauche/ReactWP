6976a84d7e85ce22647ab3c3e59621ff4d264abf
Studio Champ Gauche <comptes@champgauche.studio>
2025-01-04 07:39:35 -0500

    Change <scg-wrap /> to <rwp-wrap />

--- src/themes/reactwp/js/components/Wrapper.jsx
+++ src/themes/reactwp/js/components/Wrapper.jsx
@@ -4,7 +4,7 @@ import React from 'react';
 const Wrapper = ({ value }) => {
 
 	return(
-		<scg-wrap dangerouslySetInnerHTML={{ __html: value }} />
+		<rwp-wrap dangerouslySetInnerHTML={{ __html: value }} />
 	);
 	
 }
