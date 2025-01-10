2a81e2ca0c35f52aca265d1cc342fb173d5ede57
Studio Champ Gauche <comptes@champgauche.studio>
2025-01-10 11:25:49 -0500

    Download medias on page transition

--- src/themes/reactwp/js/utils/PageTransition.jsx
+++ src/themes/reactwp/js/utils/PageTransition.jsx
@@ -171,6 +171,8 @@ const PageTransition = ({ children }) => {
 
 	        			anchor = anchor.replace('#', '');
 
+	        			console.log(currentPathRef.current, path, location.pathname, 'yo');
+
 	        			window.gscroll ? window.gscroll.scrollTo(document.getElementById(anchor), true, 'top top') : document.getElementById(anchor).scrollIntoView({behavior: 'smooth'});
 
 	        		}
@@ -186,9 +188,159 @@ const PageTransition = ({ children }) => {
 		}
 
 		currentPathRef.current = location.pathname;
-		
-		setMiddle(false);
-		setEntering(true);
+
+
+		const isLoaded = {
+			images: false,
+			videos: false,
+			audios: false
+		};
+
+
+		const loaders = document.querySelectorAll('rwp-load');
+        const requiredLoaders = (loaders.length ? Object.keys(MEDIAS).filter((media, i) => media === loaders[i].getAttribute('data-value')) : []);
+
+        
+        let mediaDatas = [],
+        	mediaGroups = [],
+        	loadedCount = 0,
+        	totalToCount = 0;
+
+        requiredLoaders?.forEach((requiredLoader, i) => {
+
+            mediaGroups[requiredLoader] = MEDIAS[requiredLoader];
+
+        });
+
+        if(!Object.keys(mediaGroups).length){
+
+            isLoaded.images = true;
+            isLoaded.videos = true;
+            isLoaded.audios = true;
+
+            done();
+            
+            return;
+
+        }
+
+        for(let group in mediaGroups){
+
+            const medias = mediaGroups[group];
+
+            totalToCount += medias.length;
+        }
+
+
+        for(let group in mediaGroups){
+
+            const medias = mediaGroups[group];
+
+            medias.forEach(async (media, i) => {
+
+            	if(media.el){
+
+            		loaded(null, group, i);
+
+            		return;
+            	}
+
+                const mediaTypes = {
+                    video: () => document.createElement('video'),
+                    audio: () => new Audio(),
+                    image: () => new Image()
+                };
+
+                let srcElement = mediaTypes[media.type];
+
+                if(!srcElement)
+                    throw new Error(`${media.type} isn't supported.`);
+
+                srcElement = srcElement();
+
+                const cacheGet = await Cache.get(media.src);
+
+                if(!cacheGet.includes('blob:')){
+
+                    const resp = await fetch(cacheGet);
+
+                    if(!resp.ok) throw new Error(`${media.src} can\'t be loaded`);
+
+                    Cache.put(resp.url, resp.clone());
+
+
+                    const srcElementBlob = await resp.blob();
+                    const srcElementURL = URL.createObjectURL(srcElementBlob);
+
+                    srcElement.src = srcElementURL;
+                } else {
+
+                    srcElement.src = cacheGet;
+
+                }
+
+
+                loaded(srcElement, group, i);
+
+
+
+                if(['video', 'audio'].includes(media.type)){
+
+                    srcElement.preload = 'auto';
+                    srcElement.controls = true;
+                    
+                }
+
+            });
+
+        }
+
+
+        function loaded(srcElement, group, i){
+
+            loadedCount += 1;
+
+            if(srcElement)
+            	mediaGroups[group][i].el = srcElement;
+
+            if(loadedCount !== totalToCount) return;
+
+            isLoaded.images = true;
+            isLoaded.videos = true;
+            isLoaded.audios = true;
+
+            mediaDatas = mediaGroups;
+
+            done();
+
+        }
+
+
+        function done(){
+
+            if(
+                !isLoaded.images
+
+                || !isLoaded.videos
+
+                || !isLoaded.audios
+            ) return;
+            	
+
+            window.loader.medias = new Promise(resolved => {
+            	resolved({mediaGroups: MEDIAS});
+            });
+
+
+            window.loader.medias.then(() => {
+
+            	setMiddle(false);
+				setEntering(true);
+
+            });
+            
+
+        }
 		
 
 	}, [isMiddle]);
