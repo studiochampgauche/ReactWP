09220686ba0916b263d588049d75f4262ff9c5c7
Studio Champ Gauche <comptes@champgauche.studio>
2025-01-10 22:20:09 -0500

    Update Metas.jsx

--- src/themes/reactwp/js/utils/Metas.jsx
+++ src/themes/reactwp/js/utils/Metas.jsx
@@ -10,7 +10,6 @@ const Metas = ({ extraDatas, seo }) => {
 	const [ searchParams ] = useSearchParams();
 	const [isOGURL, setOGURL] = useState(null);
 	const [isFirstLoad, setFirstLoad] = useState(true);
-	const [isHelmetReady, setHelmetReady] = useState(false);
 
 	const prevPathnameRef = useRef(pathname);
 
@@ -24,8 +23,6 @@ const Metas = ({ extraDatas, seo }) => {
 
 
 		if(isFirstLoad) return;
-
-		setHelmetReady(true);
 
 		const head = document.querySelector('head');
 		const metas = head.querySelectorAll('meta[name="robots"], meta[name="description"], meta[property^="og:"]:not([property="og:site_name"]), meta[property^="article:"], meta[property^="profile:"]'); //, meta[property^="og:"]:not([property="og:site_name"]) head.querySelectorAll('meta[name="robots"], meta[name="description"], meta[property^="og:"]');
@@ -55,39 +52,37 @@ const Metas = ({ extraDatas, seo }) => {
 
 
 	return (
-		isHelmetReady && (
-			<Helmet>
-		
-				<title>{title}</title>
+		<Helmet>
 	
-				<meta name="robots" content={seo.stop_indexing || !SYSTEM.public ? 'max-image-preview:large, noindex, nofollow' : 'max-image-preview:large, index, follow'} />
-	
-				<meta property="og:type" content={seo.og_type || 'website'} />
-	
-				<meta property="og:url" content={isOGURL} />
-	
-				{description && <meta name="description" content={description} />}
-	
-				<meta property="og:title" content={og_title} />
-	
-				{og_description && <meta property="og:description" content={og_description} />}
-	
-				{og_image && <meta property="og:image" content={og_image} />}
-	
-				{seo.og_type === 'article' && [
-					(extraDatas.date && <meta key="published_time" property="article:published_time" content={extraDatas.date} />),
-					(extraDatas.modified && <meta key="modified_time" property="article:modified_time" content={extraDatas.modified} />),
-					(extraDatas.author && <meta key="author" property="article:author" content={extraDatas.author} />)
-				]}
-	
-				{seo.og_type === 'profile' && [
-					(extraDatas?.name?.firstname && <meta key="firts_name" property="profile:first_name" content={extraDatas.name.firstname} />),
-					(extraDatas?.name?.lastname && <meta key="last_name" property="profile:last_name" content={extraDatas.name.lastname} />),
-					(extraDatas?.username && <meta key="username" property="profile:username" content={extraDatas.username} />)
-				]}
-	
-			</Helmet>
-		)
+			<title>{title}</title>
+
+			<meta name="robots" content={seo.stop_indexing || !SYSTEM.public ? 'max-image-preview:large, noindex, nofollow' : 'max-image-preview:large, index, follow'} />
+
+			<meta property="og:type" content={seo.og_type || 'website'} />
+
+			<meta property="og:url" content={isOGURL} />
+
+			{description && <meta name="description" content={description} />}
+
+			<meta property="og:title" content={og_title} />
+
+			{og_description && <meta property="og:description" content={og_description} />}
+
+			{og_image && <meta property="og:image" content={og_image} />}
+
+			{seo.og_type === 'article' && [
+				(extraDatas.date && <meta key="published_time" property="article:published_time" content={extraDatas.date} />),
+				(extraDatas.modified && <meta key="modified_time" property="article:modified_time" content={extraDatas.modified} />),
+				(extraDatas.author && <meta key="author" property="article:author" content={extraDatas.author} />)
+			]}
+
+			{seo.og_type === 'profile' && [
+				(extraDatas?.name?.firstname && <meta key="firts_name" property="profile:first_name" content={extraDatas.name.firstname} />),
+				(extraDatas?.name?.lastname && <meta key="last_name" property="profile:last_name" content={extraDatas.name.lastname} />),
+				(extraDatas?.username && <meta key="username" property="profile:username" content={extraDatas.username} />)
+			]}
+
+		</Helmet>
 	)
 };
 
