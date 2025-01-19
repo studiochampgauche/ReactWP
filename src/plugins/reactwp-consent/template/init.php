a452cabb7c2b3692f86fdf22af8302007058323d
Studio Champ Gauche <comptes@champgauche.studio>
2025-01-19 08:00:06 -0500

    Enqueue styles and script + JavaScript for consent

--- src/plugins/reactwp-consent/template/init.php
+++ src/plugins/reactwp-consent/template/init.php
@@ -15,7 +15,44 @@ class Consent{
     
     function __construct(){
         
-        
+        /*
+        * Enqueue styles & scripts
+        */
+        add_action('wp_enqueue_scripts', function(){
+            
+            /*
+            * CSS
+            */
+            wp_enqueue_style('rwp-consent', plugin_dir_url('reactwp-consent/init.php') . 'assets/css/reactwp-consent.min.css', null, null, null);
+
+
+            /*
+            * JS
+            */
+            wp_enqueue_script('rwp-consent', plugin_dir_url('reactwp-consent/init.php') . 'assets/js/reactwp-consent.min.js', null, null, true);
+
+
+            /*
+            * Data
+            */
+            $consentData = \rwp::field('consent_box');
+            wp_localize_script('rwp-consent', 'RWP_CONSENT_BOX', $consentData);
+
+
+            /*
+            * Remove rwp-main script type attribute
+            */
+            add_filter('script_loader_tag', function($tag, $handle, $src){
+                if($handle !== 'rwp-consent')
+                    return $tag;
+
+                $tag = '<script src="' . esc_url( $src ) . '"></script>';
+
+                return $tag;
+
+            } , 10, 3);
+
+        }, 11);
         
     }
     
