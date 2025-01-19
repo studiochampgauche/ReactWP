4f0a6d34a45b74aff0d9eb71b096c735e3b1d168
Studio Champ Gauche <comptes@champgauche.studio>
2025-01-19 06:12:25 -0500

    Add Consent plugin without JS and CSS

---
+++ src/plugins/reactwp-consent/template/init.php
@@ -0,0 +1,23 @@
+<?php
+/*
+* Plugin Name: ReactWP Consent
+* Description: Add a consent box
+* Update URI: false
+* Version: 1.0.0
+*/
+
+
+namespace ReactWP\Consent;
+
+require_once 'inc/render.php';
+
+class Consent{
+    
+    function __construct(){
+        
+        
+        
+    }
+    
+}
+new Consent();
