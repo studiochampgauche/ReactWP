bc40555fc381e21fe63ec523a613826fb6ea09e6
Studio Champ Gauche <comptes@champgauche.studio>
2025-01-21 13:51:56 -0500

    Add Default Template

---
+++ src/themes/reactwp/js/templates/Default.jsx
@@ -0,0 +1,21 @@
+'use strict';
+import React from 'react';
+
+const Default = () => {
+	
+	return(
+		<>
+			<section id="test" style={{
+				background: '#00ff00',
+				height: '100svh'
+			}} />
+			<section style={{
+				background: '#ff0000',
+				height: '100svh'
+			}} />
+		</>
+	);
+	
+}
+
+export default Default;
