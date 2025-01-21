905b3079aa8832de7f8d67169922617aa997dce9
Studio Champ Gauche <comptes@champgauche.studio>
2025-01-21 08:01:25 -0500

    Clean demo

--- src/themes/reactwp/template/index.php
+++ src/themes/reactwp/template/index.php
@@ -3,6 +3,9 @@
 get_header();
 while(have_posts()) : the_post();
 
-
+echo '
+<section id="test" style="background: #00ff00; height: 100svh;"></section>
+<section style="background: #ff0000; height: 100svh;"></section>
+';
 
 endwhile; get_footer();
