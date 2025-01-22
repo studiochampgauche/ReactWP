acbcc55dd821df97e2f29f0c5a01535c19b93b0b
Studio Champ Gauche <comptes@champgauche.studio>
2025-01-22 07:50:43 -0500

    Add SYSTEM variable

--- src/mu-plugins/plugins/reactwp/template/init.php
+++ src/mu-plugins/plugins/reactwp/template/init.php
@@ -25,6 +25,18 @@ class ReactWP{
             */
             wp_localize_script('rwp-main', 'CL', ['value' => CL]);
 
+
+            /*
+            * System management
+            */
+            wp_localize_script('rwp-main', 'SYSTEM', [
+                'public' => get_option('blog_public'),
+                'baseUrl' => site_url(),
+                'adminUrl' => admin_url(),
+                'ajaxPath' => '/wp-admin/admin-ajax.php',
+                'restPath' => '/wp-json'
+            ]);
+
             
         }, 11);
 
@@ -90,6 +102,179 @@ class ReactWP{
                 ], 'option');
 
             }
+
+
+            /*
+            * Register some acf fields
+            */
+            $postTypes = get_post_types();
+
+            $unsets = [
+                'post',
+                'page',
+                'attachment',
+                'revision',
+                'nav_menu_item',
+                'custom_css',
+                'customize_changeset',
+                'oembed_cache',
+                'user_request',
+                'wp_block',
+                'wp_template',
+                'wp_template_part',
+                'wp_global_styles',
+                'wp_navigation',
+                'acf-field',
+                'acf-ui-options-page',
+                'acf-field-group',
+                'acf-post-type',
+                'acf-taxonomy',
+                'acf-field',
+                'wp_font_family',
+                'wp_font_face'
+            ];
+
+            foreach ($unsets as $unset) {
+                unset($postTypes[$unset]);
+            }
+
+
+            $mediaGroupsPostTypes =  rwp::field('mediaGroups_post_types') ? rwp::field('mediaGroups_post_types') : [];
+
+
+            $mediaGroupsPts = [
+                [
+                    [
+                        'param' => 'post_type',
+                        'operator' => '==',
+                        'value' => 'post',
+                    ]
+                ],
+                [
+                    [
+                        'param' => 'post_type',
+                        'operator' => '==',
+                        'value' => 'page',
+                    ]
+                ],
+                [
+                    [
+                        'param' => 'user_form',
+                        'operator' => '==',
+                        'value' => 'all',
+                    ]
+                ],
+                [
+                    [
+                        'param' => 'taxonomy',
+                        'operator' => '==',
+                        'value' => 'all',
+                    ]
+                ]
+            ];
+
+
+            if($mediaGroupsPostTypes){
+                foreach ($mediaGroupsPostTypes as $pt) {
+                    $mediaGroupsPts[] = [
+                        [
+                            'param' => 'post_type',
+                            'operator' => '==',
+                            'value' => $pt,
+                        ]
+                    ];
+                }
+            }
+
+
+
+            acf_add_local_field_group( array(
+                'key' => 'group_67kjhb39087sdh233',
+                'title' => 'Media Groups',
+                'fields' => array(
+                    array(
+                        'key' => 'field_67kjhb39087sdh234',
+                        'label' => (CL === 'fr' ? 'Groupe de médias' : 'Media Groups'),
+                        'name' => 'media_groups',
+                        'aria-label' => '',
+                        'type' => 'text',
+                        'instructions' => '',
+                        'required' => 0,
+                        'conditional_logic' => 0,
+                        'wrapper' => array(
+                            'width' => '',
+                            'class' => '',
+                            'id' => '',
+                        ),
+                        'default_value' => '',
+                        'maxlength' => '',
+                        'allow_in_bindings' => 0,
+                        'placeholder' => '',
+                        'prepend' => '',
+                        'append' => ''
+                    ),
+                ),
+                'location' => $mediaGroupsPts,
+                'menu_order' => 8,
+                'position' => 'side',
+                'style' => 'seamless',
+                'label_placement' => 'top',
+                'instruction_placement' => 'label',
+                'hide_on_screen' => '',
+                'active' => true,
+                'description' => '',
+                'show_in_rest' => 1,
+            ) );
+
+            acf_add_local_field_group( array(
+                'key' => 'acf-group_60983478kjhsad54323',
+                'title' => 'Media Groups Global Settings',
+                'fields' => array(
+                    array(
+                        'key' => 'field_60983478kjhsad54324',
+                        'label' => 'Media Groups Post Types',
+                        'name' => 'mediaGroups_post_types',
+                        'aria-label' => '',
+                        'type' => 'checkbox',
+                        'instructions' => (CL === 'fr' ? 'Les articles, les pages, les utilisateurs et les taxonomies ont le module. Si vous ne voyez pas de sélection, créez un nouveau type d\'article.' : 'Posts, pages, users and taxonomies have the module. If you don\'t see a selection, create a new post type.'),
+                        'required' => 0,
+                        'conditional_logic' => 0,
+                        'wrapper' => array(
+                            'width' => '33.3333333333',
+                            'class' => '',
+                            'id' => '',
+                        ),
+                        'choices' => $postTypes,
+                        'default_value' => array(
+                        ),
+                        'return_format' => 'value',
+                        'allow_custom' => 0,
+                        'allow_in_bindings' => 0,
+                        'layout' => 'vertical',
+                        'toggle' => 0,
+                        'save_custom' => 0,
+                        'custom_choice_button_text' => 'Add new choice',
+                    ),
+                ),
+                'location' => array(
+                    array(
+                        array(
+                            'param' => 'options_page',
+                            'operator' => '==',
+                            'value' => 'site-settings',
+                        ),
+                    ),
+                ),
+                'menu_order' => 9,
+                'position' => 'normal',
+                'style' => 'seamless',
+                'label_placement' => 'top',
+                'instruction_placement' => 'label',
+                'hide_on_screen' => '',
+                'active' => true,
+                'description' => '',
+                'show_in_rest' => 0,
+            ) );
 
 
         }, 11);
@@ -182,86 +367,6 @@ class ReactWP{
         * Shot events on acf/init action
         */
         add_action( 'acf/init', function() {
-
-
-            $postTypes = get_post_types();
-
-            $unsets = [
-                'post',
-                'page',
-                'attachment',
-                'revision',
-                'nav_menu_item',
-                'custom_css',
-                'customize_changeset',
-                'oembed_cache',
-                'user_request',
-                'wp_block',
-                'wp_template',
-                'wp_template_part',
-                'wp_global_styles',
-                'wp_navigation',
-                'acf-field',
-                'acf-ui-options-page',
-                'acf-field-group',
-                'acf-post-type',
-                'acf-taxonomy',
-                'acf-field',
-                'wp_font_family',
-                'wp_font_face'
-            ];
-
-            foreach ($unsets as $unset) {
-                unset($postTypes[$unset]);
-            }
-
-
-            $mediaGroupsPostTypes = \rwp::field('mediaGroups_post_types') ? \rwp::field('mediaGroups_post_types') : [];
-
-
-            $mediaGroupsPts = [
-                [
-                    [
-                        'param' => 'post_type',
-                        'operator' => '==',
-                        'value' => 'post',
-                    ]
-                ],
-                [
-                    [
-                        'param' => 'post_type',
-                        'operator' => '==',
-                        'value' => 'page',
-                    ]
-                ],
-                [
-                    [
-                        'param' => 'user_form',
-                        'operator' => '==',
-                        'value' => 'all',
-                    ]
-                ],
-                [
-                    [
-                        'param' => 'taxonomy',
-                        'operator' => '==',
-                        'value' => 'all',
-                    ]
-                ]
-            ];
-
-
-            if($mediaGroupsPostTypes){
-                foreach ($mediaGroupsPostTypes as $pt) {
-                    $mediaGroupsPts[] = [
-                        [
-                            'param' => 'post_type',
-                            'operator' => '==',
-                            'value' => $pt,
-                        ]
-                    ];
-                }
-            }
 
 
             /*
@@ -397,98 +502,9 @@ class ReactWP{
                 'description' => '',
                 'show_in_rest' => 0,
             ) );
-
-            
-            acf_add_local_field_group( array(
-                'key' => 'group_67kjhb39087sdh233',
-                'title' => 'Media Groups',
-                'fields' => array(
-                    array(
-                        'key' => 'field_67kjhb39087sdh234',
-                        'label' => (CL === 'fr' ? 'Groupe de médias' : 'Media Groups'),
-                        'name' => 'media_groups',
-                        'aria-label' => '',
-                        'type' => 'text',
-                        'instructions' => '',
-                        'required' => 0,
-                        'conditional_logic' => 0,
-                        'wrapper' => array(
-                            'width' => '',
-                            'class' => '',
-                            'id' => '',
-                        ),
-                        'default_value' => '',
-                        'maxlength' => '',
-                        'allow_in_bindings' => 0,
-                        'placeholder' => '',
-                        'prepend' => '',
-                        'append' => ''
-                    ),
-                ),
-                'location' => $mediaGroupsPts,
-                'menu_order' => 8,
-                'position' => 'side',
-                'style' => 'seamless',
-                'label_placement' => 'top',
-                'instruction_placement' => 'label',
-                'hide_on_screen' => '',
-                'active' => true,
-                'description' => '',
-                'show_in_rest' => 1,
-            ) );
-
-            acf_add_local_field_group( array(
-                'key' => 'acf-group_60983478kjhsad54323',
-                'title' => 'Media Groups Global Settings',
-                'fields' => array(
-                    array(
-                        'key' => 'field_60983478kjhsad54324',
-                        'label' => 'Media Groups Post Types',
-                        'name' => 'mediaGroups_post_types',
-                        'aria-label' => '',
-                        'type' => 'checkbox',
-                        'instructions' => (CL === 'fr' ? 'Les articles, les pages, les utilisateurs et les taxonomies ont le module. Si vous ne voyez pas de sélection, créez un nouveau type d\'article.' : 'Posts, pages, users and taxonomies have the module. If you don\'t see a selection, create a new post type.'),
-                        'required' => 0,
-                        'conditional_logic' => 0,
-                        'wrapper' => array(
-                            'width' => '33.3333333333',
-                            'class' => '',
-                            'id' => '',
-                        ),
-                        'choices' => $postTypes,
-                        'default_value' => array(
-                        ),
-                        'return_format' => 'value',
-                        'allow_custom' => 0,
-                        'allow_in_bindings' => 0,
-                        'layout' => 'vertical',
-                        'toggle' => 0,
-                        'save_custom' => 0,
-                        'custom_choice_button_text' => 'Add new choice',
-                    ),
-                ),
-                'location' => array(
-                    array(
-                        array(
-                            'param' => 'options_page',
-                            'operator' => '==',
-                            'value' => 'site-settings',
-                        ),
-                    ),
-                ),
-                'menu_order' => 9,
-                'position' => 'normal',
-                'style' => 'seamless',
-                'label_placement' => 'top',
-                'instruction_placement' => 'label',
-                'hide_on_screen' => '',
-                'active' => true,
-                'description' => '',
-                'show_in_rest' => 0,
-            ) );
             
             
-        } );
+        });
 
 
     }
