5e66ab2eda3ecfe70a9733b5cfc3fde311a89365
Studio Champ Gauche <comptes@champgauche.studio>
2025-01-22 07:11:43 -0500

    Clean test

--- src/themes/reactwp/template/index.php
+++ src/themes/reactwp/template/index.php
@@ -3,9 +3,5 @@
 get_header();
 while(have_posts()) : the_post();
 
-echo '
-<section id="test" style="background: #00ff00; height: 100svh;"></section>
-<section style="background: #ff0000; height: 100svh;"></section>
-';
 
 endwhile; get_footer();
