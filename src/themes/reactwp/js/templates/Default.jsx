cc2df93a77aa968d12b9b7c5ac927de19f2c68c7
Studio Champ Gauche <comptes@champgauche.studio>
2025-01-23 23:24:49 -0500

    Clean demo

--- src/themes/reactwp/js/templates/Default.jsx
+++ src/themes/reactwp/js/templates/Default.jsx
@@ -1,6 +1,5 @@
 'use strict';
 import React from 'react';
-import {Link} from 'react-router-dom';
 
 const Default = () => {
 	
@@ -11,8 +10,6 @@ const Default = () => {
 				height: '100lvh'
 			}}>
 				<span>Hello World!</span>
-				<Link to="/ok">ok-</Link>
-				<Link to="/accueil/test">test</Link>
 			</section>
 		</>
 	);
