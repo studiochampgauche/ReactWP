e9f84170cb86a2bf551f7ebb7ae9f92223da943f
Studio Champ Gauche <comptes@champgauche.studio>
2025-02-18 04:00:11 -0500

    Define environment, add comments and condition ABSPATH

--- src/core/wp-config-sample.php
+++ src/core/wp-config-sample.php
@@ -1,8 +1,14 @@
 <?php
 
+/*
+* Database connexion
+*/
 $table_prefix  = 'wp_';
 
 
+/*
+* Database connexion
+*/
 define('DB_NAME', '');
 define('DB_USER', '');
 define('DB_PASSWORD', '');
@@ -10,6 +16,10 @@ define('DB_HOST', 'localhost');
 define('DB_CHARSET', 'utf8mb4');
 define('DB_COLLATE', '');
 
+
+/*
+* Secure cookies and nonces
+*/
 define('AUTH_KEY', 'put your unique phrase here');
 define('SECURE_AUTH_KEY', 'put your unique phrase here');
 define('LOGGED_IN_KEY', 'put your unique phrase here');
@@ -19,13 +29,50 @@ define('SECURE_AUTH_SALT', 'put your unique phrase here');
 define('LOGGED_IN_SALT', 'put your unique phrase here');
 define('NONCE_SALT', 'put your unique phrase here');
 
-define('WP_DEBUG', false);
-define('WP_DEBUG_LOG', false);
-define('WP_DEBUG_DISPLAY', false);
-@ini_set('display_errors', 0);
 
-define('ABSPATH', dirname(__FILE__) . '/');
+/*
+* Define environment type
+* We'll use local, development, staging and production like wordpress defaults
+* 
+* See "development" as "local"
+*
+* Use SAVEQUERIES with caution: It stores all SQL queries, which can slow down performance and increase memory usage. Avoid using it in production.
+* 
+* In your themes and plugins, you can use wp_get_environment_type();
+*/
+define('WP_ENVIRONMENT_TYPE', 'local');
 
+if(in_array(WP_ENVIRONMENT_TYPE, ['local', 'development', 'staging'])){
+
+	define('WP_DEBUG', true);
+	define('WP_DEBUG_LOG', true);
+	define('WP_DEBUG_DISPLAY', true);
+	// define('SCRIPT_DEBUG', true);
+	// define('SAVEQUERIES', true);
+	@ini_set('display_errors', 1);
+
+} else if(WP_ENVIRONMENT_TYPE === 'production'){
+
+	define('WP_DEBUG', false);
+
+}
+
+
+/*
+* Define absolute path
+*/
+if(!defined('ABSPATH')){
+	define('ABSPATH', dirname(__FILE__) . '/');
+}
+
+
+/*
+* Change default wordpress theme
+*/
 define('WP_DEFAULT_THEME', 'reactwp');
 
+
+/*
+* Call WordPress Core Settings
+*/
 require_once(ABSPATH . 'wp-settings.php');
