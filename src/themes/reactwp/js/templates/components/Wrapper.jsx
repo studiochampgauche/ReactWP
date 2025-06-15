ce0f41725381205212520ed4180dccfaee4b4057
Studio Champ Gauche <comptes@champgauche.studio>
2025-06-15 10:35:58 -0400

    Stop use dangerouslySetInnerHTML

--- src/themes/reactwp/js/templates/components/Wrapper.jsx
+++ src/themes/reactwp/js/templates/components/Wrapper.jsx
@@ -4,7 +4,9 @@ import React from 'react';
 const Wrapper = ({ value, ...props }) => {
 
 	return(
-		<rwp-wrap {...props} dangerouslySetInnerHTML={{ __html: value }} />
+		<rwp-wrap {...props}>
+			{value}
+		</rwp-wrap>
 	);
 	
 }
