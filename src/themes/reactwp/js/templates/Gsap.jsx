487a47a7146944c08c6ea0c4e6f4510554c038ee
Studio Champ Gauche <comptes@champgauche.studio>
2025-09-06 21:48:56 -0400

    Test for GSAP. Don't give attention.

---
+++ src/themes/reactwp/js/templates/Gsap.jsx
@@ -0,0 +1,32 @@
+'use strict';
+import React, {useEffect, useRef} from 'react';
+import GsapComponentDemo from './components/GsapComponentDemo';
+
+const GsapDemo = () => {
+	
+	return(
+		<>
+			<GsapComponentDemo />
+			<GsapComponentDemo />
+			<GsapComponentDemo />
+			<GsapComponentDemo />
+			<GsapComponentDemo />
+			<GsapComponentDemo />
+			<GsapComponentDemo />
+			<GsapComponentDemo />
+			<GsapComponentDemo />
+			<GsapComponentDemo />
+			<GsapComponentDemo />
+			<GsapComponentDemo />
+			<GsapComponentDemo />
+			<GsapComponentDemo />
+			<GsapComponentDemo />
+			<GsapComponentDemo />
+			<GsapComponentDemo />
+			<GsapComponentDemo />
+		</>
+	);
+	
+}
+
+export default GsapDemo;
