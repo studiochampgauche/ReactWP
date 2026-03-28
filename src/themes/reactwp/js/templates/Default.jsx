ba626bde5f592c64720547f8a2934635483d6074
Studio Champ Gauche <comptes@champgauche.studio>
2026-03-28 18:04:51 -0400

    Adjust start example

--- src/themes/reactwp/js/templates/Default.jsx
+++ src/themes/reactwp/js/templates/Default.jsx
@@ -1,11 +1,14 @@
 'use strict';
 import React from 'react';
+import '../../scss/templates/home.scss';
 
 const Default = () => {
 	
 	return(
 		<>
-			
+			<section id="h__intro">
+				<h1>ReactWP</h1>
+			</section>
 		</>
 	);
 	
