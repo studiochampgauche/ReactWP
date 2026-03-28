3cbc0f6f92fb4c3017c521f0efc026bc244ed5fa
Studio Champ Gauche <comptes@champgauche.studio>
2026-03-28 03:52:42 -0400

    Removal of unnecessary elements on start

--- src/themes/reactwp/js/templates/Default.jsx
+++ src/themes/reactwp/js/templates/Default.jsx
@@ -5,12 +5,7 @@ const Default = () => {
 	
 	return(
 		<>
-			<section style={{
-				background: '#00ff00',
-				height: '100lvh'
-			}}>
-				<span>Hello World!</span>
-			</section>
+			
 		</>
 	);
 	
--- src/themes/reactwp/template/index.php
+++ src/themes/reactwp/template/index.php
@@ -3,10 +3,4 @@
 get_header();
 while(have_posts()) : the_post();
 
-echo '
-<section style="background: #00ff00; height: 100lvh;">
-	<span>Hello World!</span>
-</section>
-';
-
 endwhile; get_footer();
